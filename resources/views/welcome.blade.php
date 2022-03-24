e6112406627b0b4b3b02e369b9c0048176f99605
darwinpiodos <darwindivinopiodos21@gmail.com>
2022-03-24 22:01:58 +0800

    "Naka himo nakug login ug register sa babaw"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <title>Laravel</title>
 
         <!-- Fonts -->
@@ -61,6 +62,10 @@
             .m-b-md {
                 margin-bottom: 30px;
             }
+            h2{
+
+                margin-top:0px;
+            }
         </style>
     </head>
     <body>
@@ -81,8 +86,9 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel | Darwin Divino Piodos
                 </div>
+                <h2>Im excited today to make my first Laravel Project</h2>
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
@@ -96,5 +102,11 @@
                 </div>
             </div>
         </div>
+
+
+
+
+        <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     </body>
 </html>
